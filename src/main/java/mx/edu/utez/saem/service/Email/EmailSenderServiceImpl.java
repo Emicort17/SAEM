e7426d99b40ca16c788c2ce012e3dcc3251572c5
isai3 <20223tn137@utez.edu.mx>
2024-04-17 23:44:58 -0600

    this works well V2

--- src/main/java/mx/edu/utez/saem/service/Email/EmailSenderServiceImpl.java
+++ src/main/java/mx/edu/utez/saem/service/Email/EmailSenderServiceImpl.java
@@ -50,7 +50,7 @@ public class EmailSenderServiceImpl implements EmailSenderService {
     @Override
     public Boolean sendEmail_password(String toEmail){
         Optional<UserBean> foundUser = userService.findUserByEmail(toEmail);
-        if(toEmail.endsWith("p")){
+        if(toEmail.endsWith("p")){//a ver si asi
             toEmail = toEmail.substring(0, toEmail.length()-1);
         }else if(toEmail.endsWith("d")){
             toEmail = toEmail.substring(0, toEmail.length()-1);
